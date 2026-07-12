1616a84070e1f3ce12ad17481ea87005ff1f7673
Truong Nguyen <130301648+TruongNGM@users.noreply.github.com>
2026-07-12 16:54:01 +0700

    Add text change to button on click event

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementById("btn").addEventListener("click", () => {
   document.getElementById("title").textContent = "Bạn vừa click nút!";
+  document.getElementById("btn").textContent = "OK";
 });
